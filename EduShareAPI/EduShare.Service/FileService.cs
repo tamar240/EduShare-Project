4d7e99743dd2cf217c4e3810aad52023e1342466
tamar <tamar36603@gmail.com>
2025-05-29 22:20:33 +0300

    fix s3key

--- EduShareAPI/EduShare.Service/FileService.cs
+++ EduShareAPI/EduShare.Service/FileService.cs
@@ -28,7 +28,7 @@ namespace EduShare.Data.Services
         public async Task<UploadedFile> AddFileAsync(UploadedFile file,int userId)
         {
             file.OwnerId = userId;
-            file.S3Key = $"{userId}/{file.FileName}";
+            file.S3Key = file.S3Key;
 
             await _repositoryManager.Files.AddAsync(file);
             await _repositoryManager.SaveAsync();
